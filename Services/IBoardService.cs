91b178eb2bc751521b767ea3793f6baa21adc252
GVEncheva22 <GVEncheva22@codingburgas.bg>
2026-03-31 09:55:36 +0300

    Add IBoardService

---
+++ Services/IBoardService.cs
@@ -0,0 +1,15 @@
+using project_GVEncheva22.Models;
+
+namespace project_GVEncheva22.Services
+{
+    public interface IBoardService
+    {
+        Task<IEnumerable<Board>> GetAllBoardsAsync();
+        Task<Board?> GetBoardByIdAsync(int boardId);
+        Task<Board> CreateBoardAsync(Board board);
+        Task<Board?> UpdateBoardAsync(Board board);
+        Task<bool> DeleteBoardAsync(int boardId);
+
+        Task<IEnumerable<Board>> GetBoardsByUserAsync(string userId);
+    }
+}
