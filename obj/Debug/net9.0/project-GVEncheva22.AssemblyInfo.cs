390f6d2a8fbaab280d24dfd3e39d96e7f630d07f
GVEncheva22 <GVEncheva22@codingburgas.bg>
2026-04-21 09:21:43 +0300

    Redesign boards page layout

--- obj/Debug/net9.0/project-GVEncheva22.AssemblyInfo.cs
+++ obj/Debug/net9.0/project-GVEncheva22.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project-GVEncheva22")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f6d97e1b8890c2a80d3bb8ccbe888615bfa58ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69dde81f3cfb148ea16a919c64379aa169cecae5")]
 [assembly: System.Reflection.AssemblyProductAttribute("project-GVEncheva22")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project-GVEncheva22")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
