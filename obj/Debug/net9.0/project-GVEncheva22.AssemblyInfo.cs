8f5c96de0edd6db440cf97a85d92fa36eaa39ccb
GVEncheva22 <GVEncheva22@codingburgas.bg>
2026-04-22 09:13:42 +0300

    Enhance input field styling

--- obj/Debug/net9.0/project-GVEncheva22.AssemblyInfo.cs
+++ obj/Debug/net9.0/project-GVEncheva22.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project-GVEncheva22")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0feeeb4eea5ce377dd89a585c06aef277e496a2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bd58fffad09e0ae6e6ea83b48183bda60cb6dc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("project-GVEncheva22")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project-GVEncheva22")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
