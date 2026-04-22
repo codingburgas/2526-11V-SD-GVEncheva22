b8f4e21315e0cf76880b19b35631a43d9341acb8
GVEncheva22 <GVEncheva22@codingburgas.bg>
2026-04-22 10:49:27 +0300

    Final UI polish and consistency improvements

--- obj/Debug/net9.0/project-GVEncheva22.AssemblyInfo.cs
+++ obj/Debug/net9.0/project-GVEncheva22.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project-GVEncheva22")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7eb262ea32aea9f371922651e17c16cdaee4950")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+310a786b44c1114c4f330a78f9b749e0fe2e68ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("project-GVEncheva22")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project-GVEncheva22")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
